fd6d434eb036340127a66816bacc4c3e53d8c9bf
Siobko Boris <Boris_siobko@mail.ru>
2022-09-23 22:24:45 +0300

    First Commit

---
+++ ReNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace TestConsole
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
